8502e4a5ba287c0f723651d9e00c381a3345343b
SarthakK-2003 <160462697+SarthakK-2003@users.noreply.github.com>
2024-08-03 16:51:08 +0530

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -3,5 +3,3 @@ import ReactDOM from "react-dom"
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
-//  "name": "scrim-s04mqf3",
-// "description": "https://v2.scrimba.com/learn-react-c0e/~04k",
